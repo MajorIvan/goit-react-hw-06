ad795d36c3b2d42f6f68be32dd76377fed15300d
Ivan Moshak <ivanmoshak11@gmail.com>
2024-02-27 14:12:49 +0200

    updated

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
+import { selectContacts, selectFilters } from "../../redux/selectors";
 
 export default function ContactList() {
-  const storedContacts = useSelector((state) => state.contacts.items);
-  const filterValue = useSelector((state) => state.filters.name);
+  const storedContacts = useSelector(selectContacts);
+  const filterValue = useSelector(selectFilters);
 
   const visibleContacts = storedContacts.filter((contact) =>
     contact.name.toLowerCase().includes(filterValue)
